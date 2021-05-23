44662117ce6d43c15c158c752cb2e3a5fa6869c6
Lstorehouse <80744640+Lstorehouse@users.noreply.github.com>
2021-05-23 22:37:41 +0800

    Update text.sh

--- text.sh
+++ text.sh
@@ -4,10 +4,9 @@
 (
   echo "resolve_username TuringLabbot"
   echo "resolve_username LvanLamCommitCodebot"
-  sleep 1
+  sleep 5
   ### @Turing_Lab_Bot
   echo "msg Turing_Lab_Bot /help"
-  sleep 5
   echo "msg Commit_Code_Bot /help"
   echo "safe_quit"
 ) | docker exec -i telegram-cli telegram-cli -N
