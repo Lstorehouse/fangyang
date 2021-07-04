629f6dd2cf476acef4a28f453b5f47a06a06c110
Lstorehouse <80744640+Lstorehouse@users.noreply.github.com>
2021-07-04 23:27:45 +0800

    Update and rename test.sh to hz.sh

---
+++ hz.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+telegramPath=/tg-ci/tg/bin/ 
+(
+  echo "resolve_username IBCNbot_bot"
+  echo "resolve_username uurss_bot"
+  sleep 5
+echo "msg 恐龙助手 /checkin"
+echo "msg 灰姑凉 /checkin"
+echo "safe_quit"
+) | ${telegramPath}telegram-cli -D
