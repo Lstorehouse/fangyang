eaf0ece79d99e7585e131357ecb7552dff54d177
Lstorehouse <80744640+Lstorehouse@users.noreply.github.com>
2021-07-10 07:28:30 +0800

    Update hz.sh

--- hz.sh
+++ hz.sh
@@ -2,7 +2,7 @@
 telegramPath=/tg-ci/tg/bin/ 
 (
   echo "resolve_username JD_ShareCode_Bot"
-  sleep 60
+  sleep 63
 echo "msg JD_ShareCode /bean vfrrpnjhifbxegmvg2ouuwhliy&olmijoxgmjutydwiizpxj56i54ld732ircudwqq&olmijoxgmjutykr4zipkrqyjbqwogmjmrdcpniq&4npkonnsy7xi3l7gct4j4ifzzvufk4emd32ydeq&olmijoxgmjutyt26m5o7nua5uplt7fbmyopa5yi"
 echo "msg JD_ShareCode /farm 72e4e2eb3c0d44728c8204922395defc&a9b81841e4754da19e6dbf1ae5341a96&203a07fe7fc145db8ef2f74b102fa034&420ab266a8224bdc97ce637aff3ffab2&e80e2cc843a94de89e235369ddb12b31"
 echo "msg JD_ShareCode /ddfactory T0225KkcRhoY_ACCdkilk_QJJQCjVWnYaS5kRrbA&T0225KkcR0oQ8VyBJxn2lPcKcgCjVWnYaS5kRrbA&T0153Ymm_Y_PUsQBhaICjVWnYaS5kRrbA&T0225KkcRhpIp1WGJEzxwaIIdQCjVWnYaS5kRrbA&T0225KkcRhkd8waGJkmgwKIKcACjVWnYaS5kRrbA"
