8d6b1cdd152e139f06ba841cccda8d5bd87f1c60
Lstorehouse <80744640+Lstorehouse@users.noreply.github.com>
2021-07-10 07:28:54 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+LOGFILE="/tg-ci/log/qd.log"
+telegram-cli -U root -W -e "msg 恐龙助手 /checkin" >> ${LOGFILE}
+telegram-cli -U root -W -e "msg 恐龙助手 /checkin" >> ${LOGFILE}
+telegram-cli -U root -W -e "msg 恐龙助手 /checkin" >> ${LOGFILE}
