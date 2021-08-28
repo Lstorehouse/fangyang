6348be976211ee0a750355e66adba0363bfe8e64
Lstorehouse <80744640+Lstorehouse@users.noreply.github.com>
2021-08-28 23:39:47 +0800

    Update qd.sh

--- qd.sh
+++ qd.sh
@@ -5,12 +5,12 @@ telegramPath=/tg-ci/tg/bin/
   echo "resolve_username iKuuuu_VPN_bot"
   echo "resolve_username ShangwangkeOrgBot"
   echo "resolve_username cwv587bot"
-  echo "resolve_username PornembyTGBot_bot"
+  echo "resolve_username PronembyTGBot3_bot"
   sleep 63
 echo "msg 灰姑凉 /checkin"
 echo "msg iKuuuu_VPN /checkin"
 echo "msg 上网课 /checkin"
 echo "msg 卷心菜 /checkin"
-echo "msg 💋棒妹(珺珺) /checkin"
+echo "msg 💋棒小妹(囡囡) /checkin"
 echo "safe_quit"
 ) | ${telegramPath}telegram-cli -W -D
