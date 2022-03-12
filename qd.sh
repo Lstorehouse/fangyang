788bfadeba92a771f627788f37bef55ef75fcc7d
Lstorehouse <80744640+Lstorehouse@users.noreply.github.com>
2022-03-13 00:16:39 +0800

    Update qd.sh

--- qd.sh
+++ qd.sh
@@ -9,7 +9,7 @@ telegramPath=/tg-ci/tg/bin/
   echo "resolve_username cwv587bot"
   sleep 63
 echo "msg 灰姑凉 /checkin"
-echo "msg 💋粿粿 /start"
+echo "msg 💘粿粿|PornembyBot /start"
 echo "msg iKuuuu_VPN /checkin"
 echo "msg 泡芙Bot /checkin"
 echo "msg 上网课 /checkin"
